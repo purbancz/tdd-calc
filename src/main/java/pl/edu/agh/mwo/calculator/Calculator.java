69d54a2707bef49862dca7cd0c5a6a041f48e265
Piotr Urbańczyk <purbancz@d17-l427-pc-02.ipa.iisg.agh.edu.pl>
2019-04-28 15:06:50 +0200

    Initial commit

---
+++ src/main/java/pl/edu/agh/mwo/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package pl.edu.agh.mwo.calculator;
+
+public class Calculator {
+    public int add(int i, int i1) {
+        return i+i1;
+    }
+
+    public int substract(int i, int i1) {
+        return i-i1;
+    }
+
+    public int multiply(int i, int i1) {
+        return i*i1;
+    }
+
+    public float divide(int i, int i1) {
+        return i/i1;
+    }
+
+    public float average(int ...) {
+    }
+}
---
+++ src/test/java/pl/edu/agh/mwo/calculator/CalculatorTest.java
@@ -0,0 +1,61 @@
+package pl.edu.agh.mwo.calculator;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+
+    private Calculator calc;
+
+
+    @Before
+    public void createCalcuatorForTheTest() {
+        calc = new Calculator();
+    }
+
+
+    @Test
+    public void testAddition(){
+        assertEquals(4, calc.add(2,2));
+        assertEquals(3, calc.add(1,2));
+        assertEquals(0, calc.add(-2,2));
+    }
+
+
+    @Test
+    public void testSubstraction(){
+        assertEquals(0, calc.substract(2,2));
+        assertEquals(-1, calc.substract(1,2));
+        assertEquals(-4, calc.substract(-2,2));
+    }
+
+    @Test
+    public void testMultiplication(){
+        assertEquals(4, calc.multiply(2,2));
+        assertEquals(2, calc.multiply(1,2));
+        assertEquals(-4, calc.multiply(-2,2));
+    }
+
+    @Test
+    public void testDivision(){
+        assertEquals(1, calc.divide(2,2));
+        assertEquals(0.5, calc.divide(1,2));
+        assertEquals(-1, calc.divide(-2,2));
+    }
+
+//    @Test(expected = ArithmeticException.class)
+
+    @Test
+    public void testAverage(){
+        assertEquals(2, calc.average(2,2));
+        assertEquals(1.5, calc.average(1,2));
+        assertEquals(0, calc.average(-2,2));
+    }
+
+
+
+}
